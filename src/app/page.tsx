482bf44945b3cf623078a7650e405a10af4ee5d7
Carlos H Chaves <carloshumberto2110@gmail.com>
2024-08-14 10:10:24 -0300

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useRef, useState } from "react";
 import logo from '../../public/LogoDev.png';
-import { setRequestMeta } from "next/dist/server/request-meta";
 
 const totalButtons = 13;
 
@@ -39,47 +38,61 @@ const App = () => {
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
   const [modalMessage, setModalMessage] = useState<string>('');
   const modalRef = useRef<HTMLDivElement>(null);
+  const modalBackdropRef = useRef<HTMLDivElement>(null);
 
-  
   const playPopitSound = () => {
     const sound = new Audio('/popit-sound.wav');
-    sound.playbackRate = 4; // Ajuste a taxa de reprodução aqui (2 para 2x a velocidade normal)
+    sound.playbackRate = 4;
     sound.play();
   };
 
   const handleGameInit = () => {
+    setInit(true);
     setPhase(1);
     setPoppedStates(Array(totalButtons).fill(false));
     setPressedButtons(new Set());
     setLitButtons(new Set());
-    setIsTopPressed(false);
-    setIsAdvanceButtonActive(false);
     setIsAdvanceButton1Pressed(false);
     setIsAdvanceButton2Pressed(false);
-    setInit(true);
+    setTimeRemaining(5);
+    setIsModalVisible(false);
   };
 
   const resetGame = () => {
+    setInit(false);
     setPhase(1);
     setPoppedStates(Array(totalButtons).fill(false));
     setPressedButtons(new Set());
     setLitButtons(new Set());
-    setIsTopPressed(false);
-    setIsAdvanceButtonActive(false);
     setIsAdvanceButton1Pressed(false);
     setIsAdvanceButton2Pressed(false);
-    setInit(false);
+    setTimeRemaining(5);
     if (timerInterval) clearInterval(timerInterval);
-    setTimeRemaining(0);
   };
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setIsModalVisible(true);
+      setModalMessage('Perdeu!! Tempo esgotado!!');
+      resetGame();
+    }
+  }, [timeRemaining]);
+
+  useEffect(() => {
+    if (phase === 21) {
+      setIsModalVisible(true);
+      setModalMessage('Ganhou! Você completou todas as fases!');
+      resetGame();
+    }
+  }, [phase]);
 
   useEffect(() => {
     if (init) {
       const count = getButtonsToLight(phase);
       const newLitButtons = new Set(getRandomIndexes(count));
       setLitButtons(newLitButtons);
-      playPopitSound(); 
-      setTimeRemaining(5); 
+      playPopitSound();
+      setTimeRemaining(5);
       if (timerInterval) clearInterval(timerInterval);
       const intervalId = setInterval(() => {
         setTimeRemaining(prevTime => {
@@ -96,15 +109,11 @@ const App = () => {
 
   useEffect(() => {
     const allLitButtonsPressed = Array.from(litButtons).every(index => pressedButtons.has(index));
-    if (allLitButtonsPressed) {
-      setIsAdvanceButtonActive(true);
-    } else {
-      setIsAdvanceButtonActive(false);
-    }
+    setIsAdvanceButtonActive(allLitButtonsPressed);
   }, [litButtons, pressedButtons]);
 
   useEffect(() => {
-    if (isAdvanceButton1Pressed && isAdvanceButton2Pressed && isAdvanceButtonActive) {
+    if (isAdvanceButton1Pressed && isAdvanceButton2Pressed && isAdvanceButtonActive && timeRemaining > 0) {
       const timer = setTimeout(() => {
         setPhase(prev => {
           const nextPhase = prev < 20 ? prev + 1 : 1;
@@ -113,11 +122,8 @@ const App = () => {
           setPoppedStates(Array(totalButtons).fill(false));
           setLitButtons(newLitButtons);
           setPressedButtons(new Set());
-          setIsTopPressed(false);
-          setIsAdvanceButton1Pressed(false);
-          setIsAdvanceButton2Pressed(false);
-          playPopitSound(); 
-          setTimeRemaining(3); // Reinicia o tempo restante para 3 segundos
+          playPopitSound();
+          setTimeRemaining(4); 
           if (timerInterval) clearInterval(timerInterval);
           const intervalId = setInterval(() => {
             setTimeRemaining(prevTime => {
@@ -136,43 +142,14 @@ const App = () => {
     }
   }, [isAdvanceButton1Pressed, isAdvanceButton2Pressed, isAdvanceButtonActive]);
 
-  useEffect(() => {
-    if (phase === 21) {
-      setIsModalVisible(true);
-      setModalMessage('Ganhou! Você completou todas as fases!');
-      resetGame()
-    }
-  }, [phase]);
-
-  useEffect(() => {
-    if (timeRemaining === 0) {
-      setIsModalVisible(true);
-      setModalMessage('Perdeu!! Tempo esgotado!!');
-      resetGame()
-    }
-  }, [timeRemaining]);
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
-        setIsModalVisible(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
   const togglePop = (index: number) => {
     if (!litButtons.has(index)) {
       setIsModalVisible(true);
-      setModalMessage('Você pressionou um pop-it não aceso!');
-      resetGame()
+      setModalMessage('Perdeu! Pressionou o pop-it errado!');
+      resetGame();
       return;
     }
-    
+
     setPoppedStates(prevStates =>
       prevStates.map((state, i) => (i === index ? !state : state))
     );
@@ -186,69 +163,62 @@ const App = () => {
       return newButtons;
     });
     if (litButtons.has(index)) {
-      playPopitSound(); // Reproduza o som ao clicar no pop-it
+      playPopitSound();
     }
   };
 
-  const advancePhase = () => {
-    if (isAdvanceButton1Pressed && isAdvanceButton2Pressed && isAdvanceButtonActive && timeRemaining > 0 ) {
-      setPhase(prev => {
-        const nextPhase = prev < 20 ? prev + 1 : 1;
-        const count = getButtonsToLight(nextPhase);
-        const newLitButtons = new Set(getRandomIndexes(count));
-        setPoppedStates(Array(totalButtons).fill(false));
-        setLitButtons(newLitButtons);
-        setPressedButtons(new Set());
-        setIsTopPressed(false);
-        setIsAdvanceButton1Pressed(false);
-        setIsAdvanceButton2Pressed(false);
-        playPopitSound(); 
-        setTimeRemaining(4); 
-        if (timerInterval) clearInterval(timerInterval);
-        const intervalId = setInterval(() => {
-          setTimeRemaining(prevTime => {
-            if (prevTime <= 1) {
-              clearInterval(intervalId);
-              return 0;
-            }
-            return prevTime - 1;
-          });
-        }, 1000);
-        setTimerInterval(intervalId);
-        return nextPhase;
-      });
-    }
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (modalRef.current && !modalRef.current.contains(event.target as Node) &&
+          modalBackdropRef.current && !modalBackdropRef.current.contains(event.target as Node)) {
+        setIsModalVisible(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleBackdropClick = () => {
+    setIsModalVisible(false);
   };
 
   return (
     <div className="relative w-full h-screen">
-    
     {isModalVisible && (
-      <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
-        <div className="bg-rose-300 transform rotate-90 p-4 rounded-lg shadow-lg">
-        <div
-            ref={modalRef}
-            className="bg-rose-200 p-4 rounded-lg shadow-lg"
-          >
+       <div
+       ref={modalBackdropRef}
+       onClick={handleBackdropClick}
+       className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50"
+     >
+       <div
+         ref={modalRef}
+         className="bg-rose-300 transform rotate-90 p-4 rounded-lg shadow-lg"
+         onClick={(e) => e.stopPropagation()}
+       >
+          <div className="bg-rose-200 p-4 rounded-lg shadow-lg">
             <h2 className="text-lg font-semibold">{modalMessage}</h2>
           </div>
         </div>
-        </div>
-      )}
+      </div>
+    )}
+
                 
-  <header className="absolute top-0 right-0 bg-gray-600 w-full flex flex-col items-center space-y-4 p-4">
-  <div className='text-white text-sm font-semibold mb-2'>
-    Pop-it Digital
-  </div>
-  <div className='flex items-center space-x-2 text-white'>
-    <button onClick={resetGame} className="bg-red-500 transform rotate-90 rounded-md h-8 w-16">Resetar</button>
-    <button onClick={handleGameInit} className="bg-blue-500 transform rotate-90 rounded-md h-8 w-16">Iniciar</button>
-    <div className="w-6 h-6">
-      <img src={logo.src} alt="Logo" className="rounded-md transform rotate-90" />
-    </div>
-  </div>
-</header>
-      <div className="flex flex-col items-center justify-center pt-24 h-screen bg-gray-600">
+                <header className="fixed top-8 w-full bg-gray-600 flex items-center justify-center p-4 z-50">
+        <div className="flex items-center space-x-2 text-white">
+          <button onClick={resetGame} className="bg-red-500 transform rotate-90 rounded-md h-8 w-16">Resetar</button>
+          <button onClick={handleGameInit} className="bg-blue-500 transform rotate-90 rounded-md h-8 w-16">Iniciar</button>
+          <div className="flex items-center space-x-2">
+            <div className='text-white transform rotate-90 text-sm font-semibold'>
+              Pop-it Digital
+            </div>
+            <img src={logo.src} alt="Logo" className="w-8 h-8 rounded-md transform rotate-90" />
+          </div>
+        </div>
+      </header>
+      <div className="flex flex-col  items-center justify-center h-screen bg-gray-600">
       <div className="flex relative items-center">
       <button
           className={`relative px-4 py-2 w-36 h-16 ${isAdvanceButtonActive ? 'bg-rose-400 animate-pulse' : 'bg-cyan-300'} text-white border-2 border-double ${isAdvanceButtonActive ? 'border-cyan-200' : 'border-green-500'} rounded-full shadow-lg`}
